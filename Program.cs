9dd0b93a1b0caa4526e260ca8e461873a5590bcd
Anishapatel03 <71424428+Anishapatel03@users.noreply.github.com>
2020-10-08 21:35:52 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace TriangleSolver
+{
+    public class Program
+    {
+        public static int Analyze(int sideA, int sideB, int sideC)
+        {
+            int result = 0;
+            // check whether a triangle is Euilateral, Isosceles or Scalene
+            if (sideA == sideB && sideB == sideC)
+            {
+                result = 1;
+            }
+            else if ((sideA == sideB || sideA == sideC) || sideB == sideC)
+            {
+                result = 2;
+            }
+            else
+            {
+                result = 3;
+            }
+            return result;
+        }
+        public static bool checkvalue(string value)
+        {
+            int Ans;
+            if (int.TryParse(value, out Ans))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n enter side A : ");
+            int A = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\n enter side B : ");
+            int B = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\n enter side C : ");
+            int C = Convert.ToInt32(Console.ReadLine());
+
+            int FinalMatch = Program.Analyze(A, B, C);
+            switch (FinalMatch)
+            {
+                case 1:
+                    Console.WriteLine("\n Triangle is an Equilateral");
+                    break;
+                case 2:
+                    Console.WriteLine("\n Triangle is a Isosceles");
+                    break;
+                case 3:
+                    Console.WriteLine("\n Triangle is a Scalene");
+                    break;
+            }
+
+        }
+    }
+}
+
+
